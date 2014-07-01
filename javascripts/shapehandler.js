88773a3979b1dce8a1c64bf5214c61aaa48cb5c9
Prasad Surase <surase_prasad@yahoo.co.in>
2014-07-01 08:09:14 +0530

    Changed code position in shapehandler

--- javascripts/shapehandler.js
+++ javascripts/shapehandler.js
@@ -1,19 +1,8 @@
 if(window.addEventListener){
   window.addEventListener('load', function(){
     //globals
-    var context, canvas, line;
-    
-    function init(){
-      canvas = document.getElementById('canvas');
-      context = canvas.getContext('2d');
-      line = new Shape();
+    var context, canvas, shape, default_shape;
 
-      canvas.addEventListener('mousedown', evnt_canvas, false); 
-      canvas.addEventListener('mousemove', evnt_canvas, false); 
-      canvas.addEventListener('mouseup', evnt_canvas, false);
-    }
-
-    //Shape constructor function with event handlers
     function Shape(){
       this.started = false;
 
@@ -40,13 +29,24 @@ if(window.addEventListener){
       };
     }//Shape
 
+    function init(){
+      canvas = document.getElementById('canvas');
+      context = canvas.getContext('2d');
+      shape = new Shape();
+
+      canvas.addEventListener('mousedown', evnt_canvas, false); 
+      canvas.addEventListener('mousemove', evnt_canvas, false); 
+      canvas.addEventListener('mouseup', evnt_canvas, false);
+    }
+
+    //Shape constructor function with event handlers
     function evnt_canvas(evnt){
       evnt._x = evnt.layerX;
       evnt._y = evnt.layerY;
 
       //check if event handler is defined or not
-      console.log(line);
-      var func = line[evnt.type];
+      console.log(shape);
+      var func = shape[evnt.type];
       if(func){
         func(evnt);
       }
